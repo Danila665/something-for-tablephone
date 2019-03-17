93c04a52c322b67587cc937c95a92ee0baacd881
Danila <Danila@DESKTOP-FB1ITJ4>
2019-03-17 16:49:38 +0500

    Initial commit

--- tablephonemk2/src/com/tropin/tablephone/IndexController.java
+++ tablephonemk2/src/com/tropin/tablephone/IndexController.java
@@ -9,6 +9,11 @@ import com.sun.net.httpserver.HttpExchange;
 import com.tropin.tablephone.interfaces.Controller;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  *
@@ -18,10 +23,30 @@ public class IndexController implements Controller {
 
     @Override
     public void process(HttpExchange he) throws IOException {
+        
+        if (he.getRequestMethod().equals("POST")){
+            InputStream isr = he.getRequestBody();
+            Scanner s = new Scanner(isr).useDelimiter("\\A");            
+            String requestBody = s.hasNext() ? s.next() : "";
+            String decodedBody = URLDecoder.decode(requestBody, "UTF-8");
+
+            Map<String, String> result = new HashMap<>();  
+            for (String param : decodedBody.split("&")) {
+                String[] entry = param.split("=");
+                if (entry.length > 1) {
+                    result.put(entry[0], entry[1]);
+                }else{
+                    result.put(entry[0], "");
+                }
+            }
+
+            System.out.println("Decoded = " + decodedBody + "\nMap = " + result);
+        }
         String responseStr = String.join(
             "<!DOCTYPE html>",
             "<html>",
                 "<head>",
+                    "<meta charset=\"UTF-8\">",
                     "<title>Tablephone</title>",
                 "</head>",
                 "<body>",
@@ -43,7 +68,6 @@ public class IndexController implements Controller {
 
         he.sendResponseHeaders(200, responseStr.length());
         he.getResponseBody().write(responseStr.getBytes());
-        he.close();
-    }
-    
+        he.close(); 
+    } 
 }
